9355b823fe54c1ec73ec62b62a6e7300c48088e5
Elroy-D <elroydesouza@yahoo.co.uk>
2026-04-21 22:01:53 +0100

    Menu UIs Work with Splash Screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,13 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // This is for a default Play button (optional)
+    public string playSceneName;
+
+    // Play button (uses the default scene above)
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync(playSceneName);
     }
 
+    // Generic scene loader (use this for Options, Credits, etc.)
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+
+    // Quit game
     public void QuitGame()
     {
         Application.Quit();
+        Debug.Log("Game Closed"); // This shows in editor since Quit doesn't work there
     }
 }
