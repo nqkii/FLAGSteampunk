260a05f3a064670b04200e89861cfbda51e0cb48
Elroy-D <elroydesouza@yahoo.co.uk>
2026-05-04 02:35:27 +0100

    Joint UI menus and tested animations in different scene

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Running - checks if the player is moving horizontally
+        float speed = Mathf.Abs(rb.linearVelocity.x);
+        animator.SetBool("isRunning", speed > 0.1f);
+
+        // Jumping - checks vertical velocity
+        bool isJumping = Mathf.Abs(rb.linearVelocity.y) > 0.1f;
+        animator.SetBool("isJumping", isJumping);
+    }
+}
--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -1,9 +1,7 @@
 using UnityEngine;
-
 public class PlayerCamera : MonoBehaviour
 {
     public Transform player;
-
     // Update is called once per frame
     void Update()
     {
