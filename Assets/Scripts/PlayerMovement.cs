ada8a711b237db7c368df636bd908abcdf2527aa
Elroy-D <elroydesouza@yahoo.co.uk>
2026-05-08 21:45:08 +0100

    Animation WIP

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,22 +1,20 @@
 using System.Collections;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float speed;
-    public Transform player;
-
     Rigidbody rb;
     Animator animator;
-
     bool facingRight;
+
+    private float originalSpeed;
+    private float originalJumpForce;
 
     [Header("Jumping")]
     public float jumpForce = 10;
     public bool onGround;
-
     public float fallMultiplier = 2.5f;
 
     [Header("Dashing")]
@@ -29,99 +27,91 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-        animator = gameObject.GetComponent<Animator>();
+        animator = GetComponentInChildren<Animator>();
         facingRight = true;
+
+        originalSpeed = speed;
+        originalJumpForce = jumpForce;
     }
 
     void Update()
     {
-        //jump when on ground and pressing space
         if (Input.GetKeyDown(KeyCode.Space) && onGround)
         {
             onGround = false;
-            rb.linearVelocity = Vector3.up * jumpForce; 
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, 0);
         }
-        if(rb.linearVelocity.y < 0)
+
+        if (rb.linearVelocity.y < 0)
         {
             rb.linearVelocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
 
-        // dash when off ground and pressing left shift
         if (!onGround && Input.GetKeyDown(KeyCode.LeftShift))
         {
             DashAbility();
         }
 
-        //animate jump , needs adjusting, working on it!
         animator.SetFloat("Jump", rb.linearVelocity.y);
-
+        animator.SetBool("IsGrounded", onGround);
     }
 
     private void FixedUpdate()
     {
         float move = Input.GetAxis("Horizontal");
 
-        //character moves horizontally
         rb.linearVelocity = new Vector3(move * speed, rb.linearVelocity.y, 0);
-        
-        //player flips towards the moving direction
-        if(move>0 && !facingRight)
-        {
-            Flip();
-        }
-        else if(move<0 && facingRight)
-        {
-            Flip();
-        }
 
-        //idle and running animation
-        if(rb.linearVelocity == Vector3.zero)
-        {
-            animator.SetFloat("Speed", 0);
-        }
-        else if(rb.linearVelocity.x > 0)
-        {
-            animator.SetFloat("Speed", 5);
-        }
-        
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
 
+        animator.SetFloat("Speed", Mathf.Abs(rb.linearVelocity.x));
     }
 
-    //flip player
     void Flip()
     {
         facingRight = !facingRight;
-        player.transform.Rotate(0, 180.0f, 0);
+        transform.Rotate(0, 180.0f, 0);
     }
 
-    //ground check
     private void OnTriggerStay(Collider other)
     {
-        onGround = true;
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            onGround = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        onGround = false;
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            onGround = false;
+        }
     }
 
     void DashAbility()
     {
-        if(canDash)
+        if (canDash)
         {
             StartCoroutine(Dash());
         }
     }
 
-    //dash player
     IEnumerator Dash()
     {
         canDash = false;
         speed = dashSpeed;
         jumpForce = dashJumpIncrease;
+        animator.SetTrigger("Dash");
+
         yield return new WaitForSeconds(dashingTime);
-        speed = 9;
-        jumpForce = 5;
+
+        speed = originalSpeed;
+        jumpForce = originalJumpForce;
+
         yield return new WaitForSeconds(timeBetweenDashes);
         canDash = true;
     }
